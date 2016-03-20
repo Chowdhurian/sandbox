b669841656f73ddc1912825a691625900eead860
Chowdhurian <manil.chowdhury@gmail.com>
2016-03-20 15:01:28 -0700

    Solving calculator, 1 method, working 2 number addition and subtraction methods

---
+++ calculator.rb
@@ -0,0 +1,18 @@
+# Build a calculator that performs addition and subtraction
+# Create a class Calculator, which performs addition and
+# subtraction of two numbers at a time. The sample code
+# explains the expected API.
+
+# method 1: basic add and substract 2 numbers
+
+class Calculator
+  def add(a, b)
+    a + b
+  end
+
+  def subtract(a, b)
+    a - b
+  end
+end
+
+# method 2:
